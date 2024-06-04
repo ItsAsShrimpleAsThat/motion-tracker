f2449205f3dfee59ac1390ffdf0a90d205b1d1e9
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2024-06-04 11:24:00 -0700

    Camera loop

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace motion_tracker
+{
+    internal class Program
+    {
+        private const int camIndex = 0;
+        static void Main(string[] args)
+        {
+            CvInvoke.NamedWindow("cam");
+            using (Mat frame = new Mat())
+            using (VideoCapture capture = new VideoCapture(camIndex))
+                while(CvInvoke.WaitKey(1) == -1)
+                {
+                    capture.Read(frame);
+                    Image<Bgr, Byte> img = frame.ToImage<Bgr, Byte>();
+
+                    CvInvoke.Imshow("cam", frame);
+                    img.Dispose();
+                }
+        }
+    }
+}
